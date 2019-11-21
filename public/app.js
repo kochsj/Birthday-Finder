859d4ab2c23c4bc0005f891c0e685bb9ea6fadfc
Charles Glass <charlie-glass@live.com>
2019-11-21 09:43:07 -0800

    fixing conflict

--- public/app.js
+++ public/app.js
@@ -8,6 +8,24 @@ function showSpinner() {
   spinner.className = "show";
   setTimeout(() => {
     spinner.className = spinner.className.replace("show", "");
-  }, 5000);
+  }, 6000);
   //
 }
+
+
+
+$("#open-button").hover(function(){
+  $("#myForm").toggle();
+  $('#callToAction').toggle();
+});
+
+
+function closeForm() {
+  $("#myForm").toggle();
+  $('#callToAction').toggle();
+}
+
+var typed = new Typed(".typedelement", {
+  strings: ["What Happened", "On the day you were born?"],
+  typeSpeed: 40
+});
